02da7b00a0faa3b87d87ac109caab5f72522926e
samuelneves003 <Samuelneves003@gmail.com>
2026-04-06 19:22:48 -0400

    samuel:adicionei os sistema de batalha

--- PrimeiroArquivo.py
+++ PrimeiroArquivo.py
@@ -21,3 +21,37 @@ def buscar_pokemon(nome):
         "velocidade": stats["speed"]
     }
     return pokemon
+
+
+def simular_batalha(p1, p2):
+    vida1 = p1["vida"]
+    vida2 = p2["vida"]
+
+    print(f"\n Batalha: {p1['nome'].capitalize()} vs {p2['nome'].capitalize()}\n")
+
+    turno = 1
+
+    while vida1 > 0 and vida2 > 0:
+        print(f"Turno {turno}")
+
+        dano1 = max(1, p1["ataque"] - p2["defesa"])
+        dano2 = max(1, p2["ataque"] - p1["defesa"])
+
+        vida2 -= dano1
+        print(f"{p1['nome']} causou {dano1} de dano!")
+
+        if vida2 <= 0:
+            print(f"\n {p1['nome'].capitalize()} venceu!")
+            return p1["nome"]
+
+        vida1 -= dano2
+        print(f"{p2['nome']} causou {dano2} de dano!")
+
+        if vida1 <= 0:
+            print(f"\n {p2['nome'].capitalize()} venceu!")
+            return p2["nome"]
+
+        print(f"Vida {p1['nome']}: {vida1} | Vida {p2['nome']}: {vida2}\n")
+
+        turno += 1
+
